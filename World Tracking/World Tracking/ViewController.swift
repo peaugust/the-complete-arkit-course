99a2e6c9eb4df408c30cace97841f6b7d2459505
Pedro Freddi <pedro@boonto.co>
2022-09-28 15:47:15 -0300

    Add function to add nodes on screen

--- World Tracking/World Tracking/ViewController.swift
+++ World Tracking/World Tracking/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import ARKit
 
 class ViewController: UIViewController {
-
     // MARK: - IBOutlets
     
     @IBOutlet var sceneView: ARSCNView!
@@ -27,6 +26,40 @@ class ViewController: UIViewController {
             .showWorldOrigin
         ]
         self.sceneView.session.run(configuration)
+    }
+    
+    // MARK: - Private
+    
+    private func getCoordinate() -> Float {
+        return (Float.random(in: -0.9...0.9)*100).rounded() / 100
+    }
+    
+    // MARK: - IBActions
+    
+    // After finish the tutorial I've decided to implement
+    // a function to generate random coordinates.
+    @IBAction func add(_ sender: Any) {
+        let x = self.getCoordinate()
+        let y = self.getCoordinate()
+        let z = self.getCoordinate()
+        
+        let text = "coordinates: \(x), \(y), \(z)"
+        let sceneText = SCNText(string: text, extrusionDepth: 0.1)
+        sceneText.font = .systemFont(ofSize: 1)
+        let textNode = SCNNode(geometry: sceneText)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+       
+        let node = SCNNode()
+        node.geometry = SCNBox(
+            width: 0.1,
+            height: 0.1,
+            length: 0.1,
+            chamferRadius: 0
+        )
+        node.addChildNode(textNode)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue.withAlphaComponent(0.3)
+        node.position = SCNVector3(x, y, z)
+        self.sceneView.scene.rootNode.addChildNode(node)
     }
 }
 
